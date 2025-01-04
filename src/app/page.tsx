8a5181b4fbb7388a274318d2365b2bcadf603378
ajovellanos <alyssiajovellanos@gmail.com>
2025-01-04 11:48:25 -0500

    Fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -133,7 +133,7 @@ const Portfolio = () => {
           • UK AI Safety Institute - building dangerous capability evaluations<br/>
           • International Space Station, Deep Space Biology & MD Anderson Cancer Center - T-Cell Microgravity Research<br/>
           • NASA Multi-Omics & AI/ML Analysis Working Group<br/>
-          • Incoming: Oxford Alignment Research Bootcamp, Experimental Curriculum Designer @ BlueDot Impact<br/>
+          • Incoming: Oxford Alignment Research Bootcamp, Experimental Curriculum Designer for BlueDot Impact<br/>
           • Olympic Recurve Archery (aiming for 2028/2032 olympics!)<br/>
           • Annual charity payloads w/ SpaceX - engraving names & dreams of cancer patients on space missions
         </Section>
